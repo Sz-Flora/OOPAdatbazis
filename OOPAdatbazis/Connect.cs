6f4584b15794b32c088eb500f1d3447cc033dcaf
SzFlora <szabof@kkszki.hu>
2025-09-22 07:42:46 +0200

    Connect constructor is modified.

--- OOPAdatbazis/Connect.cs
+++ OOPAdatbazis/Connect.cs
@@ -17,12 +17,12 @@ namespace OOPAdatbazis
         private string _password;
         private string ConnectionString;
 
-        public Connect(string database, string user, string password)
+        public Connect(string database)
         {
             _host = "localhost";
             _database = database;
-            _user = user;
-            _password = password;
+            _user = "root";
+            _password = "";
 
             ConnectionString = $"SERVER={_host};DATABASE={_database};UID={_user};PASSWORD={_password};SslMode=None";
 

55131d79f88e64d3e2ae17083a4f8824f6f21aae
SzFlora <szabof@kkszki.hu>
2025-09-22 10:07:38 +0200

    AddNewRecord is ready

--- OOPAdatbazis/Services/ISqlStatements.cs
+++ OOPAdatbazis/Services/ISqlStatements.cs
@@ -8,7 +8,8 @@ namespace OOPAdatbazis.Services
 {
     internal interface ISqlStatements
     {
-        List<object> GetAllBooks();
+        List<object> GetAllRecords();
         object GetById(int id);
+        object AddNewRecord(object newBook);
     }
 }
